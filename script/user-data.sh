b6ef154a2b42ad269e3bd1602ec767a02c556c00
unknown <dezmondmaina@gmail.com>
2025-05-19 17:32:33 +0300

    url update

--- script/user-data.sh
+++ script/user-data.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Download and execute the actual setup script
-wget -O /tmp/hpc-setup.sh  https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/sOGfgE_Kl6JYLM1Ks84kEjWcZsHkU-n3L1wHUn4sxdmMMmeOBeRgL19wzvkUJo8j/n/fr9qm01oq44x/b/hpc/o/a2-hpc-setup.sh
+wget -O /tmp/hpc-setup.sh  https://objectstorage.eu-frankfurt-1.oraclecloud.com/p/IdbROMD3PQDJ8vebr6tYdmqzr_n_1O7gseGWoDljryNpUJA4gSDChfCFOMTbi21B/n/fr9qm01oq44x/b/hpc/o/a2-hpc-setup.sh
 chmod +x /tmp/hpc-setup.sh
 sed -i 's/\r$//' /tmp/hpc-setup.sh
 
